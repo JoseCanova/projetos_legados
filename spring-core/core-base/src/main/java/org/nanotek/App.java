05329d403b7cd31f203a2f8cfc2d777e479e6bcb
josecanova <josecanova@DESKTOP-0S0R3KQ>
2019-11-15 19:15:01 -0300

    Atualizado projeto.

---
+++ spring-core/core-base/src/main/java/org/nanotek/App.java
@@ -0,0 +1,25 @@
+package org.nanotek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.integration.annotation.IntegrationComponentScan;
+import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.config.EnableIntegrationManagement;
+
+@SpringBootApplication
+@EnableIntegration
+@IntegrationComponentScan
+@EnableIntegrationManagement
+@EnableAutoConfiguration(exclude={RabbitAutoConfiguration.class})
+public class App {
+
+	public App() {
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
