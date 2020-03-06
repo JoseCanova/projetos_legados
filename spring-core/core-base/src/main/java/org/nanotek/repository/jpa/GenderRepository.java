c9839c2ebde5a800110f8347406ce1db61bbf7b8
jose <jose@kcom>
2020-03-06 19:09:47 -0300

    Updated Model

---
+++ spring-core/core-base/src/main/java/org/nanotek/repository/jpa/GenderRepository.java
@@ -0,0 +1,8 @@
+package org.nanotek.repository.jpa;
+
+import org.nanotek.beans.entity.Gender;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenderRepository extends BaseTypeRepository<Gender>{
+}
