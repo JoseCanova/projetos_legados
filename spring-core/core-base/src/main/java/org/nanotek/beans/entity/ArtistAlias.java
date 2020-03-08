5d6fb4dc9a6af16f5213d1be02f466476062fce6
jose <jose@kcom>
2020-03-07 22:12:13 -0300

    Updated Model

--- spring-core/core-base/src/main/java/org/nanotek/beans/entity/ArtistAlias.java
+++ spring-core/core-base/src/main/java/org/nanotek/beans/entity/ArtistAlias.java
@@ -2,57 +2,118 @@ package org.nanotek.beans.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
-import org.nanotek.LongBase;
+import org.nanotek.MutableBase;
+import org.nanotek.NameBase;
 
 @SuppressWarnings("serial")
 @Entity
 @Table(name="artist_alias")
-public class ArtistAlias implements LongBase{
+public class ArtistAlias  extends EntityLongBase implements MutableBase<Long> , NameBase {
 
-	@Id
-	private Long id;
-	@Column(name="artist_id",insertable=true)
-	private Long artistId;
-	@Column(name="name_key_id",insertable=true)
-	private Long nameId;
-	@Column(name="alias",insertable=true)
-	private String alias;
-	@Column(name="sortAlias",insertable=true)
-	private String sortAlias;
-	@Column(name="locale",insertable=true)
+	@NotNull
+	@ManyToOne(optional = false)
+	@JoinTable(
+			  name = "artist_alias_join", 
+			  joinColumns = @JoinColumn(name = "artist_alias_id" , referencedColumnName = "id"), 
+			  inverseJoinColumns = @JoinColumn(name = "artist_id",referencedColumnName = "id") )
+	private Artist artist;
+
+	@NotBlank
+	@Size(min = 1 , max = 1000)
+	@Column(name="name",length=1000,nullable=false)
+	private String name;
+	
+	@NotBlank
+	@Size(min = 1 , max = 1000)
+	@Column(name="sort_name",length=1000,nullable=false)
+	private String sortName;
+	
+	@Column(name="locale",length=1000,nullable=true)
 	private String locale;
-	@Column(name="lastUpdated",insertable=true)
-	private String lastUpdated;
-
-	public ArtistAlias() {
+	
+	@ManyToOne(optional = false)
+	@JoinTable(
+			  name = "artist_alias_type_join", 
+			  joinColumns = @JoinColumn(name = "artist_alias_id" , referencedColumnName = "id"), 
+			  inverseJoinColumns = @JoinColumn(name = "alias_id",referencedColumnName = "id") )
+	private ArtistAliasType artistAliasType;
+	
+	@OneToOne(optional = true, fetch = FetchType.LAZY)
+	@JoinTable(
+			  name = "artist_alias_begin_date_join", 
+			  joinColumns = @JoinColumn(name = "artist_alias_id" , referencedColumnName = "id"), 
+			  inverseJoinColumns = @JoinColumn(name = "date_id",referencedColumnName = "id") )
+	private ArtistAliasBeginDate artistAliasBeginDate;
+	
+	@OneToOne(optional = true, fetch = FetchType.LAZY)
+	@JoinTable(
+			  name = "artist_alias_begin_date_join", 
+			  joinColumns = @JoinColumn(name = "artist_alias_id" , referencedColumnName = "id"), 
+			  inverseJoinColumns = @JoinColumn(name = "date_id",referencedColumnName = "id") )
+	private ArtistAliasEndDate artistAliasEndDate;
+	
+	
+	public ArtistAlias() {}
+	
+	
+	public ArtistAlias(@NotNull Artist artist, @NotBlank @Size(min = 1, max = 1000) String name,
+			@NotBlank @Size(min = 1, max = 1000) String sortName, String locale, ArtistAliasType artistAliasType,
+			ArtistAliasBeginDate artistAliasBeginDate, ArtistAliasEndDate artistAliasEndDate) {
+		super();
+		this.artist = artist;
+		this.name = name;
+		this.sortName = sortName;
+		this.locale = locale;
+		this.artistAliasType = artistAliasType;
+		this.artistAliasBeginDate = artistAliasBeginDate;
+		this.artistAliasEndDate = artistAliasEndDate;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	public ArtistAlias(@NotNull Long id, @NotNull Artist artist, @NotBlank @Size(min = 1, max = 1000) String name,
+			@NotBlank @Size(min = 1, max = 1000) String sortName) {
+		super(id);
+		this.artist = artist;
+		this.name = name;
+		this.sortName = sortName;
 	}
 
 	@Override
-	public Long getId() {
-		return id;
+	public void setId(Long id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
 	}
 
-	public Long getArtistId() {
-		return artistId;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public void setArtistId(Long artistId) {
-		this.artistId = artistId;
+	public Artist getArtist() {
+		return artist;
 	}
 
-	public Long getNameId() {
-		return nameId;
+	public void setArtist(Artist artist) {
+		this.artist = artist;
 	}
 
-	public void setNameId(Long nameId) {
-		this.nameId = nameId;
+	public String getSortName() {
+		return sortName;
+	}
+
+	public void setSortName(String sortName) {
+		this.sortName = sortName;
 	}
 
 	public String getLocale() {
@@ -63,29 +124,31 @@ public class ArtistAlias implements LongBase{
 		this.locale = locale;
 	}
 
-	public String getAlias() {
-		return alias;
+	public ArtistAliasType getArtistAliasType() {
+		return artistAliasType;
 	}
 
-	public void setAlias(String alias) {
-		this.alias = alias;
+	public void setArtistAliasType(ArtistAliasType artistAliasType) {
+		this.artistAliasType = artistAliasType;
 	}
 
-	public String getLastUpdated() {
-		return lastUpdated;
+	public ArtistAliasBeginDate getArtistAliasBeginDate() {
+		return artistAliasBeginDate;
 	}
 
-	public void setLastUpdated(String lastUpdated) {
-		this.lastUpdated = lastUpdated;
+	public void setArtistAliasBeginDate(ArtistAliasBeginDate artistAliasBeginDate) {
+		this.artistAliasBeginDate = artistAliasBeginDate;
 	}
 
-	public String getSortAlias() {
-		return sortAlias;
+	public ArtistAliasEndDate getArtistAliasEndDate() {
+		return artistAliasEndDate;
 	}
 
-	public void setSortAlias(String sortAlias) {
-		this.sortAlias = sortAlias;
+	public void setArtistAliasEndDate(ArtistAliasEndDate artistAliasEndDate) {
+		this.artistAliasEndDate = artistAliasEndDate;
 	}
 
-
+	
+	
+	
 }
