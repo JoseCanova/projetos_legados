3c367d8bbd5351cf02e4b490bcbc850516e83d02
jose <jose@kcom>
2020-03-11 15:19:05 -0300

    ArtistAliasTypeController...

--- spring-core/core-base/src/main/java/org/nanotek/controller/entity/ArtistAliasTypeController.java
+++ spring-core/core-base/src/main/java/org/nanotek/controller/entity/ArtistAliasTypeController.java
@@ -4,7 +4,11 @@ import org.nanotek.beans.entity.ArtistAliasType;
 import org.nanotek.service.jpa.ArtistAliasTypeJpaService;
 import org.nanotek.service.jpa.BasePersistenceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/artist_alias_type")
 public class ArtistAliasTypeController implements EntityResponseController<ArtistAliasType, Long> {
 
 	@Autowired
