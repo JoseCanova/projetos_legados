f7c3445495db17708cb293be2661d98afd128c6f
jose <jose@kcom>
2020-03-12 19:38:07 -0300

    Update top model...

---
+++ spring-core/core-base/src/main/java/org/nanotek/UUIDBase.java
@@ -0,0 +1,24 @@
+package org.nanotek;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UUIDBase extends UUIDImutableBase{
+
+	default UUID withUUID() { 
+		UUID uuid = null;
+		try { 
+				ByteArrayOutputStream bao = new ByteArrayOutputStream();
+				ObjectOutputStream oos =  new ObjectOutputStream(bao);
+				oos.writeObject(this);
+				oos.flush();
+				uuid = UUID.nameUUIDFromBytes(bao.toByteArray());
+				oos.close();
+		}catch (Exception ex) { 
+		}
+		return Optional.ofNullable(uuid).orElseThrow(BaseException::new);
+	}
+	
+}
--- spring-core/core-base/src/main/java/org/nanotek/beans/entity/LongIdSortNameEntity.java
+++ spring-core/core-base/src/main/java/org/nanotek/beans/entity/LongIdSortNameEntity.java
@@ -4,7 +4,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 
 import org.nanotek.SortedNameBase;
 
