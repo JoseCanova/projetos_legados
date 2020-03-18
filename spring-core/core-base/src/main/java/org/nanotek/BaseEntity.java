4e6dda4d9bfc420bc2edd898c6fce92d214cf82f
jose <jose@kcom>
2020-03-18 05:19:31 -0300

    updated model

--- spring-core/core-base/src/main/java/org/nanotek/BaseEntity.java
+++ spring-core/core-base/src/main/java/org/nanotek/BaseEntity.java
@@ -6,6 +6,15 @@ import org.apache.commons.collections4.Predicate;
 
 public interface BaseEntity extends IdentityPredicate<BaseEntity>{
 		default  Optional<BaseEntity> evaluate(Predicate<BaseEntity> object) {
+			boolean eval = Optional.ofNullable(object.evaluate(this)).orElseThrow(BaseException::new);
+			return eval ? Optional.of(this) : BaseEntity.empty();
+		}
+		
+		static Optional<BaseEntity> empty() {
 			return Optional.empty();
+		}
+
+		default Optional<BaseEntity> get() {
+			return Optional.of(this);
 		}
 }
