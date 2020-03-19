80cc212aa36b7095410395624d638db2c7575742
jose <jose@kcom>
2020-03-19 05:25:56 -0300

    updated model

--- spring-core/core-base/src/main/java/org/nanotek/repository/jpa/EntityBaseRepository.java
+++
@@ -1,8 +0,0 @@
-package org.nanotek.repository.jpa;
-
-import java.io.Serializable;
-
-import org.nanotek.EntityBase;
-
-public interface EntityBaseRepository<E extends EntityBase<E,ID> , ID extends Serializable> extends IdBaseRepository<E, ID>{
-}
--- spring-core/core-base/src/main/java/org/nanotek/repository/jpa/EntityNameBaseRepository.java
+++
@@ -1,9 +0,0 @@
-package org.nanotek.repository.jpa;
-
-import java.io.Serializable;
-
-import org.nanotek.IdNameBase;
-import org.nanotek.entities.MutableNameBase;
-
-public interface EntityNameBaseRepository<E extends IdNameBase<ID, ? > , ID extends Serializable> extends EntityBaseRepository<E, ID> {
-}
