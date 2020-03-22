faca36c49fcf8a490e4d870581dae22a5d31e152
jose <jose@kcom>
2020-03-22 01:39:20 -0300

    updated model

--- spring-core/core-base/src/main/java/org/nanotek/collections/StreamableArtistEntity.java
+++
@@ -1,9 +0,0 @@
-package org.nanotek.collections;
-
-import java.io.Serializable;
-
-import org.nanotek.beans.entity.Artist;
-import org.springframework.data.util.Streamable;
-
-public interface StreamableArtistEntity<K extends Artist<?>> extends Streamable<K> , Serializable{
-}
